cd51c0a5cbc5986f2c9d60b7d8d1214b7b9d8896
Ronaldo Longhi <rlonghi.dev@gmail.com>
2025-10-16 00:46:49 -0300

    Parte 1 - Empréstimos

--- Senai_BackEnd/biblioteca/principal.py
+++ Senai_BackEnd/biblioteca/principal.py
@@ -33,6 +33,8 @@ while True:
             arquivo_para_abrir = "livros.txt"
         if tipo_procura == 'leitor':
             arquivo_para_abrir = "leitores.txt"
+        if tipo_procura == 'emprestimo':
+            arquivo_para_abrir = "emprestimos.txt"
 
         with open(arquivo_para_abrir,"r") as arq:
             linha = arq.readlines()
@@ -222,7 +224,7 @@ while True:
 
     ### Cadastro de Empréstimo ####
 
-     if menu_principal == '3':
+    if menu_principal == '3':
 
         while True:
 
@@ -239,11 +241,64 @@ while True:
             if opcao == '5':
                 break
 
+            def realiza_emprestimo(registro_leitor,registro_livro):
+                
+                with open("leitores.txt","r") as arq:
+                    global ficha_leitor
+                    global ficha_livro
+                    ficha_livro = {}
+                    ficha_leitor = {}
+            
+                    for linha in arq:
+                        dicionario = eval(linha)
+                        if registro_leitor == dicionario['Registro']:
+                            ficha_leitor = dicionario
+                
+                arq.close()#Fecha arquivo
+
+                with open("livros.txt", "r") as books:
+
+                    for linha in books:
+                        dict_books = eval(linha)
+                        if registro_livro == dict_books["Registro"]:
+                            ficha_livro = dict_books
+                
+                books.close()
+
+                if ficha_leitor != None and ficha_livro != None:
+                    dict_emprestimo = {}
+                    dict_emprestimo['leitor'] = registro_leitor
+                    dict_emprestimo['livro'] = registro_livro
+                    dict_emprestimo['data'] = datetime.date.today()
+                    dict_emprestimo['Registro'] = procura_ultimo_registro('emprestimo')
+
+                else:
+                    print("Erro, verifique os códigos de registro informados e tente novamente")
+                    exit(0)
+            
+                emprestimo_str = str(dict_emprestimo)
+
+
+                with open("emprestimos.txt","at") as arq:
+                    arq.write(emprestimo_str)
+
+                arq.close()
+                    
+                ####Implementar confirmação 
+                ####Implementar demonstrativo ficha empréstimo
+
+
+
+
+
+
             if opcao == '1': # Informar Empréstimo 
+                registro_leitor = input("Digite o número de registro do Leitor")
+                registro_livro = input("Digite o número de Registro do Livro")
+                cartao_emprestimo = realiza_emprestimo(registro_leitor,registro_livro)
 
 
-
-            if opcao == '2':
+            #if opcao == '2':
 
                 
 
